512b7b93ae5540a7f407a6bf8e2af9368dbc5400
Bryan <git@singee.me>
2024-05-06 14:44:26 +0800

    error demo

--- app/src/routes/mod.rs
+++ app/src/routes/mod.rs
@@ -2,7 +2,15 @@ pub mod ping;
 
 use pavex::blueprint::{router::GET, Blueprint};
 use pavex::f;
+use pavex::http::StatusCode;
+
+use crate::configuration::DummyConfig;
 
 pub fn register(bp: &mut Blueprint) {
     bp.route(GET, "/api/ping", f!(self::ping::get));
+    bp.route(GET, "/test", f!(self::test));
+}
+
+pub fn test(_dummy_config: &DummyConfig) -> StatusCode {
+    StatusCode::OK
 }
